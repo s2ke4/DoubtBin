62741dbbbae32b5dcafc42cb3165a3c7d069ca0e
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-09-28 14:52:48 +0530

    removed comment

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -5,12 +5,6 @@ class Loading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         color: Colors.white,
-        // child:Center(
-        //   child: SpinKitRing(
-        //   color: Colors.blue,
-        //   size: 50.0,
-        // ),
-        // )
         alignment: Alignment.center,
         padding: EdgeInsets.all(10),
         child: CircularProgressIndicator(
